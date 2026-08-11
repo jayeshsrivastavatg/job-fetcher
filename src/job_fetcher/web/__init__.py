781795911567b7568a04588822cfaef7fbd9fcd3
jayeshsrivastavatg <71186456+jayeshsrivastavatg@users.noreply.github.com>
2026-08-12 01:04:24 +0530

    Register job reset UI routes

--- src/job_fetcher/web/__init__.py
+++ src/job_fetcher/web/__init__.py
@@ -1,6 +1,8 @@
 from job_fetcher.web.app import app
 from job_fetcher.web.history_web import router as history_router
+from job_fetcher.web.reset_web import router as reset_router
 
 app.include_router(history_router)
+app.include_router(reset_router)
 
 __all__ = ["app"]
